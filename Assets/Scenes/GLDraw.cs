effebc358228decf142d4fb98b4eeecd0f9332d6
takaozando <gustavo_hideo@hotmail.com>
2021-09-16 21:43:37 -0300

    Add switch case + aula 2 primitivas

--- Assets/Scenes/GLDraw.cs
+++ Assets/Scenes/GLDraw.cs
@@ -1,4 +1,6 @@
 using System;
+using System.IO;
+using System.Reflection;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +8,9 @@ using UnityEngine;
 public class GLDraw : MonoBehaviour
 {
     public Material material;
+    public int tamanho=5;
+    public int opcao=0;
+
     
     private void OnPostRender()
     {
@@ -13,23 +18,57 @@ public class GLDraw : MonoBehaviour
 		 material.SetPass(0);
          GL.Begin(GL.LINES);
 
-        GeraQuadrado(5);
-        GeraTrianguloEquilatero(5);
-        GeraHexagono(5);
-        GeraAlmaco(5);
+        switch (opcao)
+        {
+            case 1:
+            GeraQuadrado(tamanho);
+                break;
+            case 2:
+                GeraTrianguloEquilatero(tamanho);
+                break;
+            case 3:
+                GeraHexagono(tamanho);
+                break;
+            case 4:
+                GeraX(tamanho);
+                break;
+            case 5:
+                GeraAlmaco(tamanho);
+                break;
+            case 6:
+                GeraSetaDireita();
+                break;
+            case 7:
+                GeraCantoEsquerdo();
+                break;
+            case 8:
+                GeraEstrela();
+                break;
+            case 9:
+                GeraCaixaComSetasLaterais();
+                break;
+            case 10:
+                GeraQuadradoComNum7();
+                break;
+            case 11:
+                GeraHexagonoComNum4();
+                break;
+            case 12:
+                GeraEstrelaComNum1();
+                break;
+            case 13:
+                GeraPisoQuadrado();
+                break;
+            case 14:
+                GeraCubo(tamanho);
+                break;
+            case 15:
+                GeraDesenhoCasa();
+                break;
+            default:
+                break;
+        }
 
-        GeraX(5);
-        GeraSetaDireita();
-        GeraCantoEsquerdo();
-        GeraEstrela();
-        GeraCaixaComSetasLaterais();
-        GeraQuadradoComNum7();
-        GeraHexagonoComNum4();
-        GeraEstrelaComNum1();
-        GeraPisoQuadrado();
-        GeraCubo();
-        GeraDesenhoCasa();
-        
         GL.End();
         GL.PopMatrix();
 	}
@@ -63,7 +102,7 @@ public class GLDraw : MonoBehaviour
         GL.Vertex(new Vector3(2.6f, 1));
     }
 
-    private void GeraCubo()
+    private void GeraCubo(float lado)
     {
         GeraQuadrado(5);
         GL.Vertex(new Vector3(5, 0));
@@ -76,6 +115,7 @@ public class GLDraw : MonoBehaviour
         GL.Vertex(new Vector3(0, 5));
         GL.Vertex(new Vector3(1, 6));
         GL.Vertex(new Vector3(6, 6));
+
     }
 
     private void GeraPisoQuadrado()
@@ -108,10 +148,14 @@ public class GLDraw : MonoBehaviour
     private void GeraEstrelaComNum1()
     {
         GeraEstrela();
-        GL.Vertex(new Vector3(2.5f, 2));
-        GL.Vertex(new Vector3(2.5f, 4));
-        GL.Vertex(new Vector3(2.5f, 4));
-        GL.Vertex(new Vector3(2, 3.5f));
+        GL.Vertex(new Vector3(1, 3, 0));
+        GL.Vertex(new Vector3(0, 0, 0));
+
+        GL.Vertex(new Vector3(2.5f, 2.5f, 0));
+        GL.Vertex(new Vector3(2.5f, 4, 0));
+
+        GL.Vertex(new Vector3(2.5f, 4, 0));
+        GL.Vertex(new Vector3(2, 3.5f, 0));
 
     }
 
@@ -128,14 +172,7 @@ public class GLDraw : MonoBehaviour
 
     private void GeraQuadradoComNum7()
     {
-        GL.Vertex(new Vector3(0, 0));
-        GL.Vertex(new Vector3(5, 0));
-        GL.Vertex(new Vector3(5, 0));
-        GL.Vertex(new Vector3(5, 5));
-        GL.Vertex(new Vector3(5, 5));
-        GL.Vertex(new Vector3(0, 5));
-        GL.Vertex(new Vector3(0, 0));
-        GL.Vertex(new Vector3(0, 5));
+        GeraQuadrado(5);
         GL.Vertex(new Vector3(2.5f, 1.5f));
         GL.Vertex(new Vector3(3, 3));
         GL.Vertex(new Vector3(3, 3));
@@ -184,26 +221,36 @@ public class GLDraw : MonoBehaviour
 
     private void GeraEstrela()
     {
-        GL.Vertex(new Vector3(0, 0));
-        GL.Vertex(new Vector3(2.5f, 1.5f));
-        GL.Vertex(new Vector3(2.5f, 1.5f));
-        GL.Vertex(new Vector3(5, 0));
-        GL.Vertex(new Vector3(5, 0));
-        GL.Vertex(new Vector3(4, 3));
-        GL.Vertex(new Vector3(4, 3));
-        GL.Vertex(new Vector3(6, 4));
-        GL.Vertex(new Vector3(6, 4));
-        GL.Vertex(new Vector3(3.5f, 4));
-        GL.Vertex(new Vector3(3.5f, 4));
-        GL.Vertex(new Vector3(2.5f, 5));
-        GL.Vertex(new Vector3(2.5f, 5));
-        GL.Vertex(new Vector3(1.5f, 4));
-        GL.Vertex(new Vector3(1.5f, 4));
-        GL.Vertex(new Vector3(-1, 4));
-        GL.Vertex(new Vector3(-1, 4));
-        GL.Vertex(new Vector3(1, 3));
-        GL.Vertex(new Vector3(1, 3));
-        GL.Vertex(new Vector3(0, 0));
+        GL.Vertex(new Vector3(0, 0, 0));
+        GL.Vertex(new Vector3(2.5f, 2, 0));
+
+        GL.Vertex(new Vector3(2.5f, 2, 0));
+        GL.Vertex(new Vector3(5, 0, 0));
+
+        GL.Vertex(new Vector3(5, 0, 0));
+        GL.Vertex(new Vector3(4, 3, 0));
+
+        GL.Vertex(new Vector3(4, 3, 0));
+        GL.Vertex(new Vector3(6, 4, 0));
+
+        GL.Vertex(new Vector3(6, 4, 0));
+        GL.Vertex(new Vector3(3.5f, 4, 0));
+
+        GL.Vertex(new Vector3(3.5f, 4, 0));
+        GL.Vertex(new Vector3(2.5f, 6, 0));
+
+        GL.Vertex(new Vector3(2.5f, 6, 0));
+        GL.Vertex(new Vector3(1.5f, 4, 0));
+
+        GL.Vertex(new Vector3(1.5f, 4, 0));
+        GL.Vertex(new Vector3(-1, 4, 0));
+
+        GL.Vertex(new Vector3(-1, 4, 0));
+        GL.Vertex(new Vector3(1, 3, 0));
+
+        GL.Vertex(new Vector3(1, 3, 0));
+        GL.Vertex(new Vector3(0, 0, 0));
+
     }
 
     private void GeraCantoEsquerdo()
@@ -353,6 +400,39 @@ public class GLDraw : MonoBehaviour
         GL.Vertex(new Vector3(0, lado));
         GL.Vertex(new Vector3(0, 0));
 
+
+    }
+    
+    public void GeraQuadrado2(float lado)
+    {
+        GL.Vertex3(0, 0, 0);
+        GL.Vertex3(0, 2, 0);
+        GL.Vertex3(2, 2, 0);
+        GL.Vertex3(2, 0, 0);
+    }
+
+
+    public void GeraTriangulo2(float lado)
+    {
+        GL.Begin(GL.TRIANGLES);
+        GL.Vertex3(lado, 0, 0);
+        GL.Vertex3(0, lado, 0);
+        GL.Vertex3(0, lado, 0);
+    }
+
+    public void GeraCirculo(float tamanho)
+    {
+        Vector3 center = new Vector3(0, 0, 0);
+        GL.PushMatrix();
+        material.SetPass(0);
+        GL.Color(Color.red);
+        GL.Begin(GL.LINES);
+
+        for (var t = 0.0f; t < (2 * Mathf.PI); t += 0.01f)
+        {
+            Vector3 ci = (new Vector3(Mathf.Cos(t) * tamanho + center.x, Mathf.Sin(t) * tamanho + center.y, center.z));
+            GL.Vertex3(ci.x, ci.y, ci.z);
+        }
 
     }
 }
